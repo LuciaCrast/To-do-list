1e89b2c8c22050b6b54cde6613336205a8dae058
Lucía Enreda <lucia.cano@enreda.coop>
2023-10-13 13:43:41 +0200

    create title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,22 @@
-import { Box } from "@mui/material";
-import "./Title.scss";
+import { Box, Typography } from "@mui/material";
 
 export function Title() {
   return (
-    <Box className="container-title">
-      <h1 className="container-title__title">To do List</h1>
-      <p className="container-title__paragraph"> What's next...?</p>
+    <Box className="container-title" sx={{ padding: "20px" }}>
+      <Typography
+        color="primary"
+        variant="h2"
+        className="container-title__title"
+      >
+        Todo List
+      </Typography>
+      <Typography
+        color="secondary"
+        variant="subtitle1"
+        className="container-title__paragraph"
+      >
+        What's next...?
+      </Typography>
     </Box>
   );
 }
